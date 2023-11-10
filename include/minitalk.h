c27fd9f42e85d92c046782509061dc679064b5a1
mastovija <mastovija@gmail.com>
2023-11-10 19:25:17 +0100

    Update minitalk.h

--- include/minitalk.h
+++ include/minitalk.h
@@ -1,10 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgarside <jgarside@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 18:47:06 by jgarside          #+#    #+#             */
+/*   Updated: 2023/11/10 18:47:08 by jgarside         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef MINITALK
-#define MINITALK
-#define _GNU_SOURCE
+#ifndef MINITALK_H
+# define MINITALK_H
+//# define _GNU_SOURCE
 
-#include "../printf/ft_printf.h"
-#include <signal.h>
-
+# include "../printf/ft_printf.h"
+# include <signal.h>
 
 #endif
