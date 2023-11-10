6ca3e45fb3069594e8d16660c8ddadae479cd967
mastovija <mastovija@gmail.com>
2023-11-10 19:24:08 +0100

    Update client.c

--- src/client.c
+++ src/client.c
@@ -1,75 +1,90 @@
-    #include "../include/minitalk.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgarside <jgarside@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 18:46:21 by jgarside          #+#    #+#             */
+/*   Updated: 2023/11/10 18:46:24 by jgarside         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    int ft_atoi(char* str)
-    {
-        int i = 0;
-        int sign = 1;
-        int result = 0;
-        
-        while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-            i++;
-        if (str[i] == '-')
-        {
-            sign = -1;
-            i++;
-        }
-        else if (str[i] == '+')
-            i++;
-        while (str[i] >= '0' && str[i] <= '9')
-        {
-            result *= 10;
-            result += str[i] - '0';
-            i++;
-        }
-        return (result * sign);
-        
-    }
-    int char_to_signals(int pid, char c)
-    {
-        int	current_bit;
+#include "../include/minitalk.h"
 
-        current_bit = 0;
-        while (current_bit < 8)
-        {
-            if ((c & (1 << current_bit)))
-            {
-                if(kill(pid, SIGUSR1) == -1)
-                    return (1);
-            }
-            else
-            {
-                if(kill(pid, SIGUSR2) == -1)
-                    return (2);
-            }
-            usleep(500);
-            current_bit++;
-        }
-        return (0);
-    }
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
 
-    int main (int argc, char** argv)
-    {
-        if (argc == 3)
-        {
-            int pid = ft_atoi(argv [1]);
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
 
-            int i = 0;
+int	char_to_signals(int pid, char c)
+{
+	int	current_bit;
 
-            while (argv[2][i] != '\0')
-            {
-                char_to_signals(pid, argv[2][i]);
-                i++;
-            }
-            if (argv[2][i] == '\0')
-            {
-                char_to_signals(pid, '\n');
-            }
-            
-        }
-        else
-        {
-            ft_printf("Error: the input must be like: ./client PID \"message\"\n");
-            return (1);
-        }
-        return (0);
-    }
+	current_bit = 0;
+	while (current_bit < 8)
+	{
+		if ((c & (1 << current_bit)))
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				return (1);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				return (2);
+		}
+		usleep(500);
+		current_bit++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		i = 0;
+		while (argv[2][i] != '\0')
+		{
+			char_to_signals(pid, argv[2][i]);
+			i++;
+		}
+		if (argv[2][i] == '\0')
+		{
+			char_to_signals(pid, '\n');
+		}
+	}
+	else
+	{
+		ft_printf("Error: the input must be like: ./client PID \"message\"\n");
+		return (1);
+	}
+	return (0);
+}
