b409ee4b869316fbc91aed8247cd296ddf508579
mastovija <mastovija@gmail.com>
2023-11-10 19:25:06 +0100

    Update minitalk_bonus.h

--- include/minitalk_bonus.h
+++ include/minitalk_bonus.h
@@ -1,10 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgarside <jgarside@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 18:46:59 by jgarside          #+#    #+#             */
+/*   Updated: 2023/11/10 18:47:00 by jgarside         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef MINITALK_BONUS
-#define MINITALK_BONUS
-#define _GNU_SOURCE
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+//# define _GNU_SOURCE
 
-#include "../printf/ft_printf.h"
-#include <signal.h>
-
+# include "../printf/ft_printf.h"
+# include <signal.h>
 
 #endif
